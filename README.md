b6ac5861ceb808e33d026f7647787fe2497c255a
Vojtech Balak <balakvojtech@gmail.com>
2014-04-01 08:37:46 +0200

    Font is now installed with -f.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Installation
     git clone git://github.com/Remedan/dotvim.git ~/.vim
     sh ~/.vim/install.sh
 
+to install Powerline font for Airline (probably works only on ubuntu-derived distros):
+
+    sh ~/.vim/install.sh -f
+
 Plugins included
 ----------------
 
--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
-# link .vimrc
-ln -s ~/.vim/.vimrc ~/.vimrc
-# clone plugins from git
-cd ~/.vim
-git submodule init
-git submodule update
-# install powerline font
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
-fc-cache -vf ~/.fonts
-mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+if [ $# -eq 0 ]; then
+	# link .vimrc
+	ln -s ~/.vim/.vimrc ~/.vimrc
+	# clone plugins from git
+	cd ~/.vim
+	git submodule init
+	git submodule update
+fi
+
+if [ "$1" = "-f" ]; then
+	# install powerline font
+	wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+	mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
+	fc-cache -vf ~/.fonts
+	mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+fi
