3f1bd93fd46580552cf68ab8efb4b3080f5e3f5c
Vojtech Balak <balakvojtech@gmail.com>
2015-08-29 10:44:35 +0200

    Removed laptop stuff.

--- .i3/i3status_wrapper.sh
+++ .i3/i3status_wrapper.sh
@@ -4,6 +4,5 @@ i3status -c ~/.i3/i3status.conf | while :
 do
     read line
     layout=$(xkb-switch)
-    brightness=$(cat /sys/class/backlight/acpi_video0/brightness)
-    echo "  $layout |  ${brightness}% | $line" || exit 1
+    echo "  $layout |  $line" || exit 1
 done
